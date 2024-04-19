548ca4556b9f2f3fcd9c5cd90f5a5547c4a7471c
katyam1199 <167461504+katyam1199@users.noreply.github.com>
2024-04-19 18:21:58 +0300

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,10 @@
+﻿void button1_Click(object sender, EventArgs e)
+{
+    MessageBox.Show(
+        "Выберите один из вариантов",
+        "Сообщение",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+}
